1ab73d58ffb58bd63bc850270cedc37ec6a39f05
i_averyanov <iaveryanov@gmail.com>
2012-04-10 14:45:25 +0400

    add syso

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
+		System.out.println("start main program");
+		
 		System.out.println(Thread.currentThread().getName());
 		
 		Thread.sleep(60000);
